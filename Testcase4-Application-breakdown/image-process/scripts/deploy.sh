c19962851fff2870836bfdbef4d6ae563ca60fd8
hunhoffe <hunhoffe@gcm.hunhoffe-115628.escra-pg0.apt.emulab.net>
2022-01-20 15:18:04 -0700

    Increase OW function timeouts

--- Testcase4-Application-breakdown/image-process/scripts/deploy.sh
+++ Testcase4-Application-breakdown/image-process/scripts/deploy.sh
@@ -11,6 +11,9 @@
 # See the Mulan PSL v1 for more details.
 #
 
+TIMEOUT=30000 # 5 minutes (milliseconds)
+SEQUENCE_TIMEOUT=$(expr 5 \* $TIMEOUT) # Sequence timeout is 5 function timeouts
+
 if [ -z "$TESTCASE4_HOME" ]; then
     echo "$0: ERROR: TESTCASE4_HOME environment variable not set"
     exit
@@ -38,31 +41,31 @@ fi
 java -cp upload-image/target/upload-image.jar org.serverlessbench.UploadImage $image test.jpg $couchdb_url $COUCHDB_USERNAME $COUCHDB_PASSWORD $IMAGE_DATABASE
 
 echo "3. uploading functions to OpenWhisk..."
-wsk action update extractImageMetadata extract-image-metadata/target/extract-image-metadata.jar --main org.serverlessbench.ExtractImageMetadata --docker hunhoffe/java8action-imagemagic -i \
+wsk action update extractImageMetadata extract-image-metadata/target/extract-image-metadata.jar --main org.serverlessbench.ExtractImageMetadata --docker hunhoffe/java8action-imagemagic --timeout $TIMEOUT -i \
     --param COUCHDB_URL "$couchdb_url" \
     --param COUCHDB_USERNAME "$COUCHDB_USERNAME" \
     --param COUCHDB_PASSWORD "$COUCHDB_PASSWORD" \
     --param COUCHDB_DBNAME "$IMAGE_DATABASE"
  
-wsk action update transformMetadata transform-metadata/target/transform-metadata.jar --main org.serverlessbench.TransformMetadata --docker openwhisk/java8action -i
+wsk action update transformMetadata transform-metadata/target/transform-metadata.jar --main org.serverlessbench.TransformMetadata --docker openwhisk/java8action --timeout $TIMEOUT -i
 
-wsk action update handler handler/target/handler.jar --main org.serverlessbench.Handler --docker openwhisk/java8action -i \
+wsk action update handler handler/target/handler.jar --main org.serverlessbench.Handler --docker openwhisk/java8action --timeout $TIMEOUT -i \
     --param COUCHDB_URL "$couchdb_url" \
     --param COUCHDB_USERNAME "$COUCHDB_USERNAME" \
     --param COUCHDB_PASSWORD "$COUCHDB_PASSWORD" \
     --param COUCHDB_LOGDB "$IMAGE_DATABASE_LOG"
 
-wsk action update thumbnail thumbnail/target/thumbnail.jar --main org.serverlessbench.Thumbnail --docker hunhoffe/java8action-imagemagic -i \
+wsk action update thumbnail thumbnail/target/thumbnail.jar --main org.serverlessbench.Thumbnail --docker hunhoffe/java8action-imagemagic --timeout $TIMEOUT -i \
     --param COUCHDB_URL "$couchdb_url" \
     --param COUCHDB_USERNAME "$COUCHDB_USERNAME" \
     --param COUCHDB_PASSWORD "$COUCHDB_PASSWORD" \
     --param COUCHDB_DBNAME "$IMAGE_DATABASE"
 
-wsk action update storeImageMetadata  store-image-metadata/target/store-image-metadata.jar --main org.serverlessbench.StoreImageMetadata --docker openwhisk/java8action -i \
+wsk action update storeImageMetadata  store-image-metadata/target/store-image-metadata.jar --main org.serverlessbench.StoreImageMetadata --docker openwhisk/java8action --timeout $TIMEOUT -i \
     --param COUCHDB_URL "$couchdb_url" \
     --param COUCHDB_USERNAME "$COUCHDB_USERNAME" \
     --param COUCHDB_PASSWORD "$COUCHDB_PASSWORD" \
     --param COUCHDB_DBNAME "$IMAGE_DATABASE"
 
-wsk action update imageProcessSequence --sequence extractImageMetadata,transformMetadata,handler,thumbnail,storeImageMetadata -i
+wsk action update imageProcessSequence --sequence extractImageMetadata,transformMetadata,handler,thumbnail,storeImageMetadata --timeout $SEQUENCE_TIMEOUT -i
 
