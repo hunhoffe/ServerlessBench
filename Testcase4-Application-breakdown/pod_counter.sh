d6c83ab6d877cdea66a0936dc0cea7b3fae26e32
hunhoffe <hunhoffe@gcm.hunhoffe-116175.escra-pg0.apt.emulab.net>
2022-01-29 21:33:44 -0700

    Updated pod watcher

--- Testcase4-Application-breakdown/pod_counter.sh
+++ Testcase4-Application-breakdown/pod_counter.sh
@@ -2,8 +2,9 @@
 
 while true
 do
-  num_total=$(kubectl get pods -n openwhisk -o wide | grep extractimage | wc -l)
-  num_running=$(kubectl get pods -n openwhisk -o wide | grep extractimage | grep " Running " | wc -l)
-  echo $num_total, $num_running
-  sleep .5
+    num_total=$(kubectl get pods -n openwhisk -o wide | grep extractimage | wc -l)
+    num_running=$(kubectl get pods -n openwhisk -o wide | grep extractimage | grep " Running " | wc -l)
+    current_time=$(date +%s%N)
+    echo $current_time, $num_total, $num_running
+    sleep .25
 done
